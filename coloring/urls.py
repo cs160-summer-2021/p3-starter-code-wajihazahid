43f727b750b499748fcce75aa5bd687bab0bd163
Yechan Kim <elliotkim@berkeley.edu>
2021-07-15 10:59:39 +0900

    proj3 final

--- coloring/urls.py
+++ coloring/urls.py
@@ -2,6 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('demo', views.index, name='demo'),
-    path('new_interaction', views.index, name='new_interaction')
+    # path('demo', views.index, name='demo'),
+    # path('new_interaction', views.index, name='new_interaction')
+
+    path('demo', views.demo, name='demo'),
+    path('index', views.index, name='index'),
+    path('new_interaction', views.new_interaction, name='new_interaction'),
+    path('home_screen', views.home_screen, name='home_screen'),
+    path('timer_screen', views.timer_screen, name='timer_screen'),
+    path('timer_screen_update', views.timer_screen_update, name='timer_screen_update'),
+    path('template_1', views.template_1, name='template_1'),
+    path('template_2', views.template_2, name='template_2')
+
 ]
--- coloring/views.py
+++ coloring/views.py
@@ -2,3 +2,24 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'coloring/index.html')
+
+def demo(request):
+    return render(request, 'coloring/demo.html')
+
+def new_interaction(request):
+    return render(request, 'coloring/new_interaction.html')
+
+def home_screen(request):
+    return render(request, 'coloring/home_screen.html')
+
+def timer_screen(request):
+    return render(request, 'coloring/timer_screen.html')
+
+def timer_screen_update(request):
+    return render(request, 'coloring/timer_screen_update.html')
+
+def template_1(request):
+    return render(request, 'coloring/template_1.html')
+
+def template_2(request):
+    return render(request, 'coloring/template_2.html')
